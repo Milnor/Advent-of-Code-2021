e4d516d65c9fcecaf515cb617e08f82f33610604
Seaver Milnor <milnor@users.noreply.github.com>
2021-12-03 00:13:34 +0100

    Day 1 Part 1 in C++ works (but ugly)

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,58 @@
-//#define _XOPEN_SOURCE 500
-
+#include <cstdio>
+#include <fstream>
 #include <iostream>
-//#include <cwchar>
-//#include <stdio.h>
-//#include <wchar.h>
+#include <string>
+
 using namespace std;
 
+/*
 class Submarine {
     public:
         int x;      // horizontal coordinate
         int y;      // depth coordinate
         int aim;
 };
+*/
+
+struct Submarine {
+    int x = 0;
+    int y = 0;
+    int aim = 0;
+
+    Submarine() {
+        printf("We all live in a yellow submarine\n");
+    }
+};
 
 int main() {
 
-    Submarine yellow;   // obvious Beatles pun
-    yellow.x = 0;       // move to constructor (after I learn how)!
-    yellow.y = 0;
-    yellow.aim = 0;
+    fstream challenge;
+    challenge.open("./data/01depth.txt", ios::in);
+    int * depths;
+    depths = new int [2000];
+    int i = 0;
+    int count = 0;
+    string current_data;
+    while(getline(challenge, current_data)) {
+        //cout << current_data << "\n";
+        depths[i] = stoi(current_data);
+        i++;
+    }
+    for (i = 1; i < 2000; i++)
+    {
+        if (depths[i] > depths[i - 1])
+        {
+            count++;
+        }
+    }
+    challenge.close();
+
+    cout << "Day 1 Challenge 1=" << count << "\n";
+
+    Submarine yellow{};   // obvious Beatles pun
+    //yellow.x = 0;       // move to constructor (after I learn how)!
+    //yellow.y = 0;
+    //yellow.aim = 0;
 
     cout << "x=" << yellow.x << "\n";
     cout << "y=" << yellow.y << "\n";
