5cd1fb4ecbf86723c8935025ae577bbcf62d8c4e
Seaver Milnor <milnor@users.noreply.github.com>
2021-12-09 02:36:53 +0100

    WIP tests

---
+++ tests/test_all.cpp
@@ -0,0 +1,35 @@
+#include <assert.h>
+
+int solve_d1p1(string path) {
+    return 0;
+}
+
+int solve_d1p2(string path) {
+    return 0;
+}
+
+int solve_d2p1(string path) {
+    return 0;
+}
+
+int solve_d2p2(string path) {
+    return 0;
+}
+
+int main() {
+    // Day 1
+    // Samples: 7, 5
+    // Actual: 1482, 1518
+    assert(solve_d1p1() == 7);
+    assert(solve_d1p1() == 1482);
+    assert(solve_d1p2() == 5);
+    assert(solve_d1p2() == 1518);
+
+    // Day 2
+    // Samples: 150, 900
+    // Actual: 1728414, 1765720035
+    assert(solve_d2p1() == 150);
+    assert(solve_d2p1() == 1728414);
+    assert(solve_d2p2() == 900);
+    assert(solve_d2p2() == 1765720035);
+}
